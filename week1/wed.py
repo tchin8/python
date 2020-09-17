347aaf2a1da35fcc73954d4ec6231405071e7860
Tiffany <chin.tiffany108@gmail.com>
2020-09-16 21:52:40 -0400

    first python game

--- week1/wed.py
+++ week1/wed.py
@@ -325,11 +325,114 @@ print(find_digit_amount(0))             #> 1
 
 #----------------------------------EXAMPLE--------------------------------------
 
+# Write your function, here.
+def track_robot(dirs):
+  coord = [0, 0]
+  if not len(dirs):
+    return coord
+  
+  for i in range(0, len(dirs)):
+    dir, num = dirs[i].split()
+    if dir == 'right':
+        coord[0] += int(num)
+    elif dir == 'left':
+      	coord[0] -= int(num)
+    elif dir == 'up':
+      coord[1] += int(num)
+    else:
+      coord[1] -= int(num)
+  return coord
+
+# # Write your function, here.
+# def track_robot(instructions):
+#     totals = {'left': 0, 'right': 0, 'up': 0, 'down': 0}
+#     for step in instructions:
+#         step = step.split()
+#         totals[step[0]] += int(step[1])
+#     return [totals['right'] - totals['left'], totals['up'] - totals['down']]
+
+
+print(track_robot(["right 10", "up 50", "left 30", "down 10"]))
+# Prints [-20, 40]
+
+print(track_robot([]))
+# Prints [0, 0]
+# If there are no instructions, the robot doesn't move.
+
+print(track_robot(["right 100", "right 100", "up 500", "up 10000"]))
+# Prints [200, 10500]
 
 
 #----------------------------------EXAMPLE--------------------------------------
 
+# STEP 1: Complete analyze function to return 5 values:
+#    - minimum
+#    - maximum
+#    - mean (a.k.a. average)
+#    - median (center point)
+#    - mode (most repeated)
+def analyze(nums):
+    return (
+        min(nums),
+        max(nums),
+        sum(nums)/len(nums),
+        median(nums),
+        mode(nums)
+    )
 
+# STEP 2: Complete median function to return center number
+#         WITHOUT using built-in function
+def median(nums):
+    # if there are no numbers, then exit right away
+    if (len(nums) == 0):
+        return 0
+
+    # sort numbers and find the middle
+    orderedNums = sorted(nums)
+    midIndex = len(orderedNums) // 2
+
+    # if there's an odd number, use the number in the middle
+    if (len(orderedNums) % 2 == 1):
+        return(orderedNums[midIndex])
+
+    # if there's an even number, use average of two elements in the middle
+    return((orderedNums[midIndex] + orderedNums[midIndex-1]) / 2)
+
+# STEP 3: Complete mode function to return most-repeated number
+#         WITHOUT using built-in function
+def mode(nums):
+    # build a dictionary to track the element counts
+    lookup = dict.fromkeys(nums, 0)
+    for x in nums:
+        lookup[x] += 1
+
+    # make parallel arrays of the numbers (elements) and counts
+    counts = list(lookup.values())
+    elements = list(lookup.keys())
+
+    # find the largest count and its position
+    highCount = max(counts)
+    highAt = counts.index(highCount)
+
+    # BONUS B: Catch special case where more than one value repeats the most
+    counts.sort()
+    if (len(nums) > 1 and (len(counts) == 1 or counts[-2] == highCount)):
+        return
+
+    # mode is the number corresponding to the highest count
+    return(elements[highAt])
+
+# DO NOT EDIT - sample data for checking your work
+sample1 = 1,2,3,4,5,6,7,8,9
+sample2 = [37,45,23,65,75,34,23,23,23,65,12,99]
+
+print(('min', 'max', 'mean', 'median', 'mode'))
+print(analyze(sample1))
+print(analyze(sample2))
+
+# BONUS A: Print more samples as you see fit
+print(analyze([0]))
+print(analyze([1,1,1,1,1]))
 
 
 #----------------------------------EXAMPLE--------------------------------------
