5169438265b372d8787048edce854a08251453d0
Tiffany <chin.tiffany108@gmail.com>
2020-09-30 22:15:40 -0400

    knights travail

--- week1/thu-python-knights-travail-master/tree.py
+++ week1/thu-python-knights-travail-master/tree.py
@@ -3,10 +3,23 @@ class Node:
     self._value = value
     self._parent = None
     self._children = []
+  
+  def add_child(self, node):
+    if node not in self._children:
+      self._children.append(node)
+      if node.parent != self:
+        node.parent = self
+  
+  def remove_child(self, node):
+    if node and node in self._children:
+      self._children.remove(node)
+      node.parent = None
 
+  @property
   def value(self):
     return self._value
 
+  @property
   def children(self):
     return self._children
 
@@ -15,19 +28,20 @@ class Node:
     return self._parent
   
   @parent.setter
-  def parent(self, value):
-    self._parent = value
-    # calls the 'add_child' method of the parent node passing itself as the node 
-    # to add to the list of children
-  
-  def add_child(self, node):
-    self._children.append(node)
-    # and update the node's '_parent' if the node is not already in '_children'
-  
-  def remove_child(self, node):
-    self._children.remove(node)
-    self._parent = None
-    
-  
+  def parent(self, node):
+    if self._parent != None and self in self._parent.children:
+      self._parent.remove_child(self)
+    self._parent = node
+    if node != None:
+      node.add_child(self)
 
-  
+
+# node1 = Node("root1")
+# node2 = Node("root2")
+# node3 = Node("root3")
+
+# node3.parent = node1
+# node3.parent = node2
+
+# print(node1.children)
+# print(node2.children)
