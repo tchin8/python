76669a4a09c1429036f658babdf01fea6536f6d8
Tiffany <chin.tiffany108@gmail.com>
2020-10-17 16:19:20 -0400

    flask wtforms

--- week2/day2/explore-flask/app/routes/main.py
+++ week2/day2/explore-flask/app/routes/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect
-from app import app
+from app.forms.login import LoginForm
 
 bp = Blueprint('', __name__)
 
@@ -8,12 +8,13 @@ def index():
   # return '<h1>Sample App</h1><h2>Welcome</h2>'
   return render_template('page.html', title='Welcome')
 
-# @bp.route('/login', methods=['GET', 'POST'])
-# def login():
-#   form = LoginForm()
-#   if form.validate_on_submit():
-#     return redirect('/')
-#   return render_template('login.html', form=form)
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    # return redirect('/')
+    return 'Submit complete'
+  return render_template('login.html', form=form)
 
 @bp.route('/help')
 def help():
