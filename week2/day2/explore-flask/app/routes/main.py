a420a46c3b4958a8418b9664523da5e9c6f5146a
Tiffany <chin.tiffany108@gmail.com>
2020-10-18 21:17:17 -0400

    flask session

--- week2/day2/explore-flask/app/routes/main.py
+++ week2/day2/explore-flask/app/routes/main.py
@@ -1,12 +1,33 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, session
 from app.forms.login import LoginForm
 
 bp = Blueprint('', __name__)
 
+
+def track_views():
+  if 'views' in session:
+    session['views'] = session.get('views') + 1
+  else:
+    session['views'] = 1
+
+
 @bp.route('/')
 def index():
   # return '<h1>Sample App</h1><h2>Welcome</h2>'
+  track_views()
   return render_template('page.html', title='Welcome')
+
+
+@bp.route('/views')
+def views():
+  return f'Total views: {session.get("views")}'
+
+
+@bp.route('/views/reset')
+def reset_views():
+  views = session.pop('views', None)  
+  return f'Reset views (previous {views})'
+
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -16,7 +37,9 @@ def login():
     return 'Submit complete'
   return render_template('login.html', form=form)
 
+
 @bp.route('/help')
 def help():
   # return '<h1>Sample App</h1><h2>Help</h2>'
+  track_views()
   return render_template('page.html', title='Help')
