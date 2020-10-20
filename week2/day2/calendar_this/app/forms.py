eb8bbe0b8fd22c2f41b8b91fc125bdc501a3a0ac
Tiffany <chin.tiffany108@gmail.com>
2020-10-20 14:54:22 -0400

    flask deltatime, datetime

--- week2/day2/calendar_this/app/forms.py
+++ week2/day2/calendar_this/app/forms.py
@@ -1,16 +1,28 @@
+from datetime import datetime
 from flask_wtf import FlaskForm
 # from wtforms.fields import BooleanField, DateField, StringField, SubmitField, TextAreaField, TimeField    # this is ugly, so use below instead
 from wtforms.fields import (
   BooleanField, DateField, StringField, SubmitField, TextAreaField, TimeField
 )
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from wtforms.widgets.html5 import DateInput, TimeInput
 
 class AppointmentForm(FlaskForm):
-  name = StringField("Name")
-  start_date = DateField("Start_date")
-  start_time = TimeField("Start_time")
-  end_date = DateField("End_date")
-  end_time = TimeField("End_time")
-  description = TextAreaField("Description")
-  private = BooleanField("Private?")
+  name = StringField("Name", [DataRequired()])
+  start_date = DateField("Start date", [DataRequired()], widget=DateInput())
+  start_time = TimeField("Start_time", [DataRequired()], widget=TimeInput())
+  end_date = DateField("End_date", [DataRequired()], widget=DateInput())
+  end_time = TimeField("End_time", [DataRequired()], widget=TimeInput())
+  description = TextAreaField("Description", [DataRequired()])
+  private = BooleanField("Private?", [DataRequired()])
   submit = SubmitField("Create an appointment")
+
+  def validate_end_date(self, form, field):
+    start = datetime.combine(form.start_date.data, form.start_time.data)
+    end = datetime.combine(field.data, form.end_time.data)
+    if start >= end:
+      msg = "End date/time must come after start date/time"
+      raise ValidationError(msg)
+    if form.start_date.data != form.end_date.data:
+      msg = "End date must be the same as start date"
+      raise ValidationError(msg)
--- week2/day2/calendar_this/app/routes.py
+++ week2/day2/calendar_this/app/routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
-import psycopg2
-import os
+from datetime import datetime, timedelta
+from flask import Blueprint, render_template, redirect, url_for
 from .forms import AppointmentForm
+import os
+import psycopg2
 
 bp = Blueprint("main", __name__, "")
 
@@ -13,17 +14,62 @@ CONNECTION_PARAMETERS = {
 }
 
 @bp.route("/")
+def main():
+  d = datetime.now()
+  return redirect(url_for(".daily", year=d.year, month=d.month, day=d.day))
+
+
+@bp.route("/<int:year>/<int:month>/<int:day>", methods=["GET", "POST"])
 # def main():
 #   # return "<h2>Calendar working</h2>"
 #   return render_template('main.html')
-def main():
+def daily(year, month, day):
   form = AppointmentForm()
+  if form.validate_on_submit():
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+      with conn.cursor() as curs:
+        sql = """
+          INSERT INTO appointments (
+            name,
+            start_datetime,
+            end_datetime,
+            description,
+            private
+          )
+          VALUES
+          (
+            %(name)s,
+            %(start_datetime)s,
+            %(end_datetime)s,
+            %(description)s,
+            %(private)s
+          )
+        """
+      params = {
+        'name': form.name.data,
+        'start_datetime': datetime.combine(
+                          form.start_date.data,
+                          form.start_time.data,
+                          ),
+        'end_datetime': datetime.combine(
+                          form.end_date.data,
+                          form.end_time.data,
+                        ),
+        'description': form.description.data,
+        'private': form.private.data
+      }
+      curs.execute(sql, params)
+      return redirect("/")
   with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
     with conn.cursor() as curs:
-      curs.execute("""
-                  SELECT name, start_datetime, end_datetime
-                  FROM appointments
-                  ORDER BY start_datetime;
-                  """)   # took out id bc didnt need it
+      day = datetime(year, month, day)
+      next_day = day + timedelta(days=1)
+      sql = """
+        SELECT id, name, start_datetime, end_datetime
+        FROM appointments
+        WHERE start_datetime BETWEEN %(day)s AND %(next_day)s
+        ORDER BY start_datetime
+      """
+      curs.execute(sql, {'day': day, 'next_day': next_day})
       rows = curs.fetchall()
       return render_template("main.html", rows=rows, form=form)
